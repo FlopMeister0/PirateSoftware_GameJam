0964d60ce08b166761f42e7d3221de8b919e5d6f
FlopMeister0 <elijahpearson.010@gmail.com>
2025-01-18 17:15:51 +0000

    Update. Tilemapping

--- graphics/Tiled/tilemap_packed.tsx
+++ graphics/Tiled/tilemap_packed.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.1" name="tilemap_packed" tilewidth="16" tileheight="16" tilecount="198" columns="18">
- <image source="../piratesoftware_gamejam/PirateSoftware_GameJam/graphics/Tilemap/tilemap_packed.png" width="288" height="176"/>
+ <image source="../Tilemap/tilemap_packed.png" width="288" height="176"/>
 </tileset>
