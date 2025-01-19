6a6aff2c7c5d8b35fe747c87b0ea350fb8baf432
FlopMeister0 <elijahpearson.010@gmail.com>
2025-01-19 10:30:55 +0000

    Stripping it down

--- TanksAlot.py
+++ TanksAlot.py
@@ -1,6 +1,5 @@
 import pygame
-from Tiles import *
-from spritesheet import Spritesheet
+import pytmx
 from pytmx.util_pygame import load_pygame
 
 class Player(pygame.sprite.Sprite):
@@ -31,18 +30,20 @@ class Player(pygame.sprite.Sprite):
         
 class background(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__()
-    
+        scale_factor = 2
+
     def blit_all_tiles(window, tmxdata):
+        scale_factor = 2
         for layer in tmxdata:
-            for tile in layer.tiles():
+            for x, y, surf in layer.tiles():
                 # tiles[0] = x grid location
                 # tiles[1] = y grid location
                 # tile[2] = image data for blitting
-                x_pixel = tile[0] * 70   
-                y_pixel = tile[1] * 70 
-                window.blit(tile[2], (x_pixel, y_pixel))    
-
+                #scaled_surf = pygame.transform.scale_by(surf, scale_factor)
+                x_pixel = x * 16
+                y_pixel = y * 16
+                window.blit(surf, (x_pixel, y_pixel))  
+            
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
@@ -53,14 +54,13 @@ running = True
 # spritesheet = spritesheet.parse_sprite()
 # player_Rect = player_img.get_rect()
 
+"""Loading Level"""
+tmx_data = load_pygame('graphics/Tiled/Gamejam.tmx')
+# player_rect.x, player_rect.y = map.start_x, map.start_y
+
 """Player Character"""
 player = pygame.sprite.GroupSingle()
 player.add(Player())
-
-"""Loading Level"""
-tmx_data = load_pygame('graphics/Tiled/Gamejam.tmx')
-world_offset = [0,0]
-# player_rect.x, player_rect.y = map.start_x, map.start_y
 
     
 """Game Loop"""
@@ -80,10 +80,9 @@ while True:
         background.blit_all_tiles(screen, tmx_data)
         player.draw(screen)
         player.update()
-    # else:
-    #     screen.fill("Green")
+    else:
+        screen.fill("Green")
 
     
     pygame.display.flip()
     clock.tick(60)
-    # map.draw_map(screen)
