dd2f072285a48b49b56199e35b72a0a8dfddf053
FlopMeister0 <elijahpearson.010@gmail.com>
2025-01-23 10:58:57 +0000

    Sounds and menu items

--- TanksAlot.py
+++ TanksAlot.py
@@ -46,33 +46,41 @@ class StartButton(pygame.sprite.Sprite):
         background.reset_game()
 
 class Text():
+    def Background():
+        menu = pygame.image.load("graphics/Menu/Menu.png")
+        screen.blit(menu, (0,140))
+    
     def title():
         outline_color = "Black"
         for dx,dy in [(-3,0), (3,0), (0,-3), (-3,-3), (3,-3), (-3,3), (3,3)]:
             outline_title = Font_countdown.render('TanksALOT', True, outline_color)
             scaled_by = pygame.transform.scale_by(outline_title, 1.5)
-            screen.blit(scaled_by, (420 + dx, 250 + dy))
+            screen.blit(scaled_by, (420 + dx, 150 + dy))
             
         title_text = Font_countdown.render('TanksALOT', True, ("Green"))
         scaled_title = pygame.transform.scale_by(title_text, 1.5)
-        screen.blit(scaled_title,(420,250))
+        screen.blit(scaled_title,(420,150))
     
     def instructions():
-        instruction_text = Font_countdown.render('Press WASD to Move & Space to Shoot!', True, ("Green"))
-        screen.blit(instruction_text,(120,350))
-        # instruction_text = Font_countdown.render('Space to Shoot!', True, ("Green"))
-        # screen.blit(instruction_text,(400,400))
+        instruction_text = Font_countdown.render('Press WASD to Move & Right-Click to Shoot!', True, ("Lime"))
+        screen.blit(instruction_text,(30,250))
+        game_text1 = Font_countdown.render('Destroy all objects in the stage', True, ("Lime"))
+        screen.blit(game_text1,(30,375))
+        game_text2 = Font_countdown.render('without getting hit by your own bullet', True, ("Lime"))
+        screen.blit(game_text2,(30,425))
+        game_text3 = Font_countdown.render('or running out of time to win!', True, ("Lime"))
+        screen.blit(game_text3,(30,475))
         
     def countdown():
         # outline
         outline_color = "black"
         # dx and dy are to offset the outline from the text, such as (-2,0) shifts 2 pixels to the left. whilst (-2,-2) handle diagonal
         for dx,dy in [(-3,0), (3,0), (0,-3), (-3,-3), (3,-3), (-3,3), (3,3)]:
-            outline_text = Font_countdown.render(f'{counter}', True, outline_color)
+            outline_text = Font_countdown.render(f'Time: {counter}', True, outline_color)
             screen.blit(outline_text, (10 + dx, 0 + dy))
         
         # countdown
-        Countdown_text = Font_countdown.render(f'{counter}', True, ("turquoise"))
+        Countdown_text = Font_countdown.render(f'Time: {counter}', True, ("turquoise"))
         screen.blit(Countdown_text,(10,0))
     
 class Tile(pygame.sprite.Sprite):
@@ -107,9 +115,13 @@ class background(pygame.sprite.Sprite):
                     Tile(pos = pos, surf = surf, groups = self.tiles)
         
         for obj in self.tmx_data.objects:
+            objects_left = obj
             pos = (obj.x, obj.y) # grabbing the position of the object
             obj_type = obj.name if hasattr(obj,"name") else "Unknown" # if defined assign the object
             Gameobject(pos=pos, surf=obj.image, groups=self.objects, obj_type = obj_type) # ensure each object has a name
+            
+        "print(objects_left)
+        
         
     def draw(self, window):
         self.tiles.draw(window)
@@ -134,21 +146,24 @@ class Bullet(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2(direction) # direction to move in (1 for right, -1 for left)
         self.speed = speed
 
-    def collision(self, background_obj):
+    def collision(self, background_obj, bullet_group):
         collided_obj = pygame.sprite.spritecollide(self, background_obj, False)
         if collided_obj:
                 for obj in collided_obj:
                     if obj.type != "Water":
                      background_obj.remove(obj)
+                     hit_sound = pygame.mixer.Sound("audio/sound_effects/Hit.wav")
+                     hit_sound.set_volume(0.08)
+                     hit_sound.play()
                      self.kill()
-                    elif self.rect.x < 0 or self.rect.x > 1280:
+                    elif self.rect.x < 0 or self.rect.x > 1280 or self.rect.y < 0 or self.rect.y > 720:
                      self.kill() # kills if it leaves the screen
-        
+            
     def update(self, background_obj):
         # move the bullet in a direction
         self.rect.x += self.direction.x * self.speed
         self.rect.y += self.direction.y * self.speed
-        self.collision(background_obj)
+        self.collision(background_obj, bullet_group)
 
 
 class Player(pygame.sprite.Sprite):
@@ -162,9 +177,8 @@ class Player(pygame.sprite.Sprite):
         self.previous_pos = self.rect.topleft
         self.speed = 2
         self.movement = pygame.math.Vector2(0,0) # initilize movement
-        self.bullets = pygame.sprite.Group() # Group of bullets
         self.direction = "right"
-        self.shooting_cooldown = 500
+        self.shooting_cooldown = 300
         self.last_shot = pygame.time.get_ticks()
     
     def player_input(self):
@@ -196,15 +210,16 @@ class Player(pygame.sprite.Sprite):
         current_time = pygame.time.get_ticks()
         
         if current_time - self.last_shot >= self.shooting_cooldown:
+            bullet_offset = 20
             if self.direction == "up":
-                bullet = Bullet(self.rect.centerx, self.rect.top, direction=(0,-1), speed=4, image='graphics/Tiles/tile_0191-up.png')
+                bullet = Bullet(self.rect.centerx, self.rect.top - bullet_offset, direction=(0,-1), speed=4, image='graphics/Tiles/tile_0191-up.png')
             elif self.direction == "down":
-                bullet = Bullet(self.rect.centerx, self.rect.bottom, direction=(0,1), speed=4, image='graphics/Tiles/tile_0191-down.png')
+                bullet = Bullet(self.rect.centerx, self.rect.bottom + bullet_offset, direction=(0,1), speed=4, image='graphics/Tiles/tile_0191-down.png')
             elif self.direction == "left":
-                bullet = Bullet(self.rect.left, self.rect.centery, direction=(-1,0), speed=4, image='graphics/Tiles/tile_0191-left.png')
+                bullet = Bullet(self.rect.left - bullet_offset, self.rect.centery, direction=(-1,0), speed=4, image='graphics/Tiles/tile_0191-left.png')
             elif self.direction == "right":
-                bullet = Bullet(self.rect.right, self.rect.centery, direction=(1,0), speed=4, image='graphics/Tiles/tile_0191-right.png')
-            self.bullets.add(bullet)
+                bullet = Bullet(self.rect.right + bullet_offset, self.rect.centery, direction=(1,0), speed=4, image='graphics/Tiles/tile_0191-right.png')
+            bullet_group.add(bullet)
             self.last_shot = current_time # update to the current time.
             
     def collision(self, background_obj):
@@ -213,18 +228,30 @@ class Player(pygame.sprite.Sprite):
                 for obj in collided_obj:
                     if obj.type != "Water":
                      background_obj.remove(obj)
+                     hit_sound = pygame.mixer.Sound("audio/sound_effects/Powerup.wav")
+                     hit_sound.set_volume(0.05)
+                     hit_sound.play()
                      self.speed += 0.1 # increase speed of tank
                     elif obj.type == "Water":
                         self.rect.topleft = self.previous_pos
                         self.movement.x = 0
                         self.movement.y = 0
+        
+        hit_by_bullet = pygame.sprite.spritecollide(self, bullet_group, True) # kills player when set to true
+        if hit_by_bullet:
+            hit_sound = pygame.mixer.Sound("audio/sound_effects/Hit.wav")
+            hit_sound.set_volume(0.08)
+            hit_sound.play()
+            global game_active, victory
+            victory = False
+            game_active = False
                 
         if self.rect.x < 0 or self.rect.x > 1280 or self.rect.y < 0 or self.rect.y > 720:
             self.rect.topleft = self.previous_pos
             self.movement.x = 0
             self.movement.y = 0
                         
-    def update(self, background_obj):
+    def update(self, background_obj, bullet_group):
         self.previous_pos = self.rect.topleft
         
         self.player_input()
@@ -234,7 +261,7 @@ class Player(pygame.sprite.Sprite):
         # Update the collision with the object background
         self.collision(background_obj)
         # Update the bullets
-        for bullet in self.bullets:
+        for bullet in bullet_group:
          bullet.update(background_obj)
 
             
@@ -246,8 +273,11 @@ running = True
 victory = None
 restart = None
 
+"""Bullets"""
+bullet_group = pygame.sprite.Group()
+
 """Buttons"""
-start_button = StartButton(pos=(400,450), action=StartButton.start_game)
+start_button = StartButton(pos=(400,550), action=StartButton.start_game)
 button_group = pygame.sprite.Group(start_button)
 
 """Gray Overlay"""
@@ -294,9 +324,8 @@ while True:
             pygame.quit()
             exit()
             
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shooting()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+            player.shooting()
         
         elif event.type == timer:
             if game_active == True:
@@ -311,11 +340,16 @@ while True:
     if game_active == True: 
         Background.draw(screen)
         player_group.draw(screen)
-        player_group.update(Background.objects)
-        player.bullets.draw(screen)
+        player_group.update(Background.objects, bullet_group)
+        bullet_group.draw(screen)
         Text.countdown()
         
     else:
+        Background.draw(screen)
+        screen.blit(gray_overlay,(0,0))
+        button_group.update()
+        button_group.draw(screen)
+
         if victory == True:
             screen.blit(yellow_overlay,(0,0))
             button_group.draw(screen)
@@ -329,13 +363,9 @@ while True:
             game_active = True
             restart = False
         
-        Background.draw(screen)
-        screen.blit(gray_overlay,(0,0))
-        button_group.update()
-        button_group.draw(screen)
+        Text.Background()
         Text.instructions()
         Text.title()
-
     
     pygame.display.flip()
     clock.tick(60)
